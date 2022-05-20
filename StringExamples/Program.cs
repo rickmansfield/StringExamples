cac1280d35e72a6ca9ca7185384c3ececb7f1730
Rick Mansfield <PEACEFULRICK@msn.com>
2022-05-20 18:30:53 -0500

    Sec 9 Vid 124 Indexing

--- StringExamples/Program.cs
+++ StringExamples/Program.cs
@@ -8,27 +8,45 @@ namespace StringExamples
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //string coursName = "Learn C# for Beginners Crash Course";
+        //string message = "Welcom to ";
+
+        //// Concatenate courseName and message to create a new string
+        //string fullMessage = message + coursName;
+        //Console.WriteLine(fullMessage);
+
+        //// Use string interpolation 
+        //string fullMessage2 = $"{message}{coursName}";
+        //Console.WriteLine(fullMessage2);
+
+        //// Use Composite Formatting with mehtods that support it
+        //Console.WriteLine("Hello, and {0} the {1}", message, coursName );
+
+        //for (int i = 0; i < coursName.Length; i++)
+        //{
+        //    char character = coursName[i];
+        //    Console.WriteLine(character);
+        //}
+        //===============================
+
         static void Main(string[] args)
         {
+            //0         1         2         3  
+            //0123456789012345678901234567890123456789
             string coursName = "Learn C# for Beginners Crash Course";
             string message = "Welcom to ";
 
-            // Concatenate courseName and message to create a new string
-            string fullMessage = message + coursName;
-            Console.WriteLine(fullMessage);
+            Console.WriteLine(coursName[0]);
+            Console.WriteLine(coursName[7]);
 
-            // Use string interpolation 
-            string fullMessage2 = $"{message}{coursName}";
-            Console.WriteLine(fullMessage2);
-
-            // Use Composite Formatting with mehtods that support it
-            Console.WriteLine("Hello, and {0} the {1}", message, coursName );
-
-            for (int i = 0; i < coursName.Length; i++)
+            for(int i = 13; i < 21; i++)
             {
-                char character = coursName[i];
-                Console.WriteLine(character);
+                Console.Write(coursName[i]);
             }
+            Console.WriteLine();
+            
         }
     }
 }
