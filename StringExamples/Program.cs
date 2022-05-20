300469971a2ee82d8df349f5044d41825c95f20f
Rick Mansfield <PEACEFULRICK@msn.com>
2022-05-20 18:14:02 -0500

    Add project files.

---
+++ StringExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string coursName = "Learn C# for Beginners Crash Course";
+            string message = "Welcom to ";
+
+            // Concatenate courseName and message to create a new string
+            string fullMessage = message + coursName;
+            Console.WriteLine(fullMessage);
+
+            // Use string interpolation 
+            string fullMessage2 = $"{message}{coursName}";
+            Console.WriteLine(fullMessage2);
+
+            // Use Composite Formatting with mehtods that support it
+            Console.WriteLine("Hello, and {0} the {1}", message, coursName );
+
+            for (int i = 0; i < coursName.Length; i++)
+            {
+                char character = coursName[i];
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
