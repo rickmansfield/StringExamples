d2000e88652b71fe54c97caa4318b4ac356efb93
Rick Mansfield <PEACEFULRICK@msn.com>
2022-06-05 13:08:48 -0500

    Sec 9 Video 129

--- StringExamples/Program.cs
+++ StringExamples/Program.cs
@@ -46,7 +46,7 @@ namespace StringExamples
         //        Console.Write(coursName[i]);
         //    }
         //    Console.WriteLine();
-            
+
         //}
 
         //==========Sec 9 video 125============
@@ -73,24 +73,43 @@ namespace StringExamples
         //==================Sec 9 video 127=============
         //static void Main(string[] args)
         //{
-            //                  0         10's      20's      30's 
-            //                  0123456789012345678901234567890123456789
-            //string coursName = "Learn C# for Beginners Crash Course";
-            //int position;
+        //                  0         10's      20's      30's 
+        //                  0123456789012345678901234567890123456789
+        //string coursName = "Learn C# for Beginners Crash Course";
+        //int position;
 
-           /* position = coursName.IndexOf(" c", StringComparison.OrdinalIgnoreCase);*/// treats the characters in the strings to compare as if they were converted to uppercase using the conventions of the invariant culture, and then performs a simple byte comparison that is independent of language.
+        /* position = coursName.IndexOf(" c", StringComparison.OrdinalIgnoreCase);*/// treats the characters in the strings to compare as if they were converted to uppercase using the conventions of the invariant culture, and then performs a simple byte comparison that is independent of language.
 
-            /* position = coursName.IndexOf(" c", StringComparison.CurrentCultureIgnoreCase);*///ignores case & used when strings are linguistically 
-            // see alos https://docs.microsoft.com/en-us/dotnet/api/system.stringcomparison?view=net-6.0
-            //Console.WriteLine(position);
-        //}
+        /* position = coursName.IndexOf(" c", StringComparison.CurrentCultureIgnoreCase);*///ignores case & used when strings are linguistically 
+                                                                                           // see alos https://docs.microsoft.com/en-us/dotnet/api/system.stringcomparison?view=net-6.0
+                                                                                           //Console.WriteLine(position);
+                                                                                           //}
 
         //==================Sec 9 video 128=============
+        //static void Main(string[] args)
+        //{
+        //    //                  0         10's      20's      30's 
+        //    //                  0123456789012345678901234567890123456789
+        //    string coursName = "Learn C# for Beginners Crash Course";
+        //    int position = -1;
+
+        //    //position = coursName.IndexOf(" c", StringComparison.OrdinalIgnoreCase);
+        //    //position = coursName.LastIndexOf(" c", StringComparison.OrdinalIgnoreCase);
+        //    do
+        //    {
+
+        //    position = coursName.IndexOf(" c", position + 1, StringComparison.OrdinalIgnoreCase);
+        //    Console.WriteLine(position);
+        //    }
+        //    while (position != -1);
+        //}
+
+        //==================Sec 9 video 129==========================
         static void Main(string[] args)
         {
             //                  0         10's      20's      30's 
             //                  0123456789012345678901234567890123456789
-            string coursName = "Learn C# for Beginners Crash Course";
+            string courseName = "Learn C# for Beginners Crash Course";
             int position = -1;
 
             //position = coursName.IndexOf(" c", StringComparison.OrdinalIgnoreCase);
@@ -98,10 +117,21 @@ namespace StringExamples
             do
             {
 
-            position = coursName.IndexOf(" c", position + 1, StringComparison.OrdinalIgnoreCase);
-            Console.WriteLine(position);
+                position = courseName.IndexOf(" c", position + 1, StringComparison.OrdinalIgnoreCase);
+                if (position != -1)
+                {
+                    courseName = ReplaceByIndex(courseName, position, " c".Length, " Java");
+                    Console.WriteLine(courseName);
+                }
             }
             while (position != -1);
+            string fixedString = courseName.Replace(" Java", " C");
+            Console.WriteLine(fixedString);
+        }
+        private static string ReplaceByIndex(string original, int start, int length, string replacement)
+        {
+            string newString = original.Remove(start, length);
+            return newString.Insert(start, replacement);
         }
     }
 }
